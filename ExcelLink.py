370d95f1203ea65024302699796b8924bcf33f40
cihanandac <67173940+cihanandac@users.noreply.github.com>
2021-11-02 11:17:12 +0300

    Update ExcelLink.py

--- ExcelLink.py
+++ ExcelLink.py
@@ -10,14 +10,15 @@ from openpyxl.drawing.image import Image
 import tkinter as tk
 from tkinter import filedialog
 
-print(" "* 2900)
+"""This program is created to serve the need of giving automatic links of photos to a file, it will 
+search a column of your choice sheet by sheet and if it finds any match in photo pools location, link 
+will be given to cell of your choice.
+Don't seperate the photo pool folder with the excel afterwards.
+
+Feel free to use and share.
+"""
+
 print("Welcome to ExcelLink") 
-print("This program is written for the purpose of linking excel cells")
-print("with the related photos.") 
-print("        ")
-print("Feel free to use and share.")
-print("Made by Cihan Andac")
-print(" "* 1000)
 print("You will be asked to choose the Excel file first and then")
 print("the directories of the photos for each sheet.")
 print(" "* 900)
@@ -27,18 +28,15 @@ print(" "* 900)
 root = tk.Tk()
 root.withdraw()
 
+#This is the excel file you want to add the links.
 file_path = filedialog.askopenfilename(title="Choose the excel file")
 print(file_path)
-
-
 file = panda.ExcelFile(file_path)
 sheets = file.sheet_names
-
 wb = load_workbook(file_path)
 
+#This is the directory which contains the photos that we will give links to.
 directory_path = filedialog.askdirectory(title="Choose the location of the photo pool")
-
-
 
 #iterating through the excel file for sheets
 for sheet in sheets:
@@ -55,12 +53,12 @@ for sheet in sheets:
         photo_check = page['Inv. No.'][i]
   
         #checking if there is a match
+        #The algorithm for searching the filename is created according the need of the developer.
         
         for filename in os.listdir(sheet_path):
-
             
+            #If your photo's name and the cell have the same name simply delete the codes until two lines above the Eureka part.
             first_line=0
-            
             for k in range(0,len(filename)):
                 if filename[k] == "_":
                     if first_line==0:
@@ -71,28 +69,26 @@ for sheet in sheets:
                             if filename[j] == "_" or filename[j]== ".":
                                 if second_line ==0 or second_line == 1:
                                     second_line = second_line + 1
-
                             
                                 elif second_line ==2:
                                     shm_number = "SHM "+ filename[k+1:j]
                                     
-
+                                    #This means that the photo's name is the same with the cell.
                                     if shm_number == photo_check:
-                                        print("eureka!!")
+                                        print("Eureka!!")
+                                        
+                                        #Feel free to change the column that you want to add the link.
+                                        ws.cell(row=i+2, column=2).value = '=HYPERLINK("{}", "{}")'.format("Object_Photo/"+os.path.basename(directory_path)+"/"+sheet+"/"+ filename, "file")
+                                        
+                                        
+                                        """ if you have the thumbnails for the photos you can also add them to another cell with following codes
                                         if os.path.isfile('Thumbnails/'+filename):
                                             img = openpyxl.drawing.image.Image('Thumbnails/'+filename+".jpg")
                                             img.anchor = "B"+str(i+2)
-                                            ws.cell(row=i+2, column=2).value = '=HYPERLINK("{}", "{}")'.format("Object_Photo/"+os.path.basename(directory_path)+"/"+sheet+"/"+ filename, "file")
                                             ws.add_image(img)
-                                        else:
-                                            ws.cell(row=i+2, column=2).value = '=HYPERLINK("{}", "{}")'.format("Object_Photo/"+os.path.basename(directory_path)+"/"+sheet+"/"+ filename, "file")
                                         
-                                    
-                                        
-                                                                            
-        
+                                        """
 
-                    
 wb.save(file_path)
 
 
